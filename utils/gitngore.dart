e2881968dc8b2feb1296dc4b4ce958ad335c52c7
Mohamed25885 <memo_868@yahoo.com>
2025-01-11 19:40:02 +0200

    add pvm to gitignore

---
+++ utils/gitngore.dart
@@ -0,0 +1,40 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'utils.dart';
+
+class GitIgnore {
+  final Directory directory;
+
+  GitIgnore(String dir) : this.directory = Directory(dir);
+
+  Future<bool> checkExistence() async {
+    if (await Directory(directory.path + "\\.git").exists()) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  Future<void> add(String line) async {
+    if (!(await checkExistence())) {
+      return;
+    }
+    //add line to gitignore
+    File file = File(directory.path + "\\.gitignore");
+    if (file.existsSync()) {
+      // Read the file line by line
+      final lines = file.openRead().transform(utf8.decoder).transform(LineSplitter());
+
+      // Process each line
+      await for (final line in lines) {
+        if (line == Utils.directoryName) {
+          return;
+        }
+      }
+      file.writeAsStringSync("\n"+line, mode: FileMode.append);
+    } else {
+      file.writeAsStringSync(line);
+    }
+  }
+}
--- utils/option_creator.dart
+++ utils/option_creator.dart
@@ -1,14 +1,23 @@
 import 'dart:io';
 
+import 'gitngore.dart';
 import 'symlink_creator.dart';
 import 'utils.dart';
 
 class OptionCreator {
   static Future<({String from, String to})> createLocal(String version) async {
-    final homeDir = Platform.environment['HOME'] ?? Platform.environment['USERPROFILE'];
     final programDirectory = File(Platform.script.toFilePath()).parent;
+    final gitIgnore = GitIgnore(programDirectory.path);
 
-    return await SymlinkCreator.createSymLink(version, "${programDirectory.path}\\versions\\PHP${version}",
+    if (await gitIgnore.checkExistence()) {
+      print("Do you want to add .pvm to gitignore? (y\\n)");
+      var answer = stdin.readLineSync();
+      if (answer?.toLowerCase() == "y") {
+        await gitIgnore.add(Utils.directoryName);
+      }
+    }
+
+    return await SymlinkCreator.createSymLink(version, "${programDirectory.path}\\versions\\php${version}",
         "${Directory.current.path}\\${Utils.directoryName}");
   }
 
@@ -17,6 +26,6 @@ class OptionCreator {
     final programDirectory = File(Platform.script.toFilePath()).parent;
 
     return await SymlinkCreator.createSymLink(
-        version, "${programDirectory.path}\\versions\\PHP${version}", "$homeDir\\${Utils.directoryName}");
+        version, "${programDirectory.path}\\versions\\php${version}", "$homeDir\\${Utils.directoryName}");
   }
 }
